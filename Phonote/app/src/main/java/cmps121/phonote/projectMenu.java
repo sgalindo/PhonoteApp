dda7686af18b1a73181bc656deb48ea89376d12e
unknown <jclouse@ucsc.edu>
2018-04-26 22:19:35 -0700

    implemented the next activity projectMenu. When you click ok, it brings you to the new activity and creates a new directory with the given name as well as sub directories citations and nots. It also displays the newly created directory under Projects.

--- Phonote/app/src/main/java/cmps121/phonote/projectMenu.java
+++ Phonote/app/src/main/java/cmps121/phonote/projectMenu.java
@@ -11,12 +11,28 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
+import java.io.File;
 
 public class projectMenu extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.project_menu);
         Bundle bundle = getIntent().getExtras();
-        String name_of_project = bundle .getString("name_of_project");
-
+        if(bundle.getBoolean("projectCreated")) {
+            String name_of_project = bundle.getString("name_of_project");
+            File new_file = new File("/projects", "/" + name_of_project);
+            boolean success = true;
+            if (!new_file.exists()) {
+                success = new_file.mkdirs();
+                File citations = new File("/"+name_of_project, "/citations");
+                citations.mkdirs();
+                File notes = new File("/"+name_of_project,"/notes");
+                notes.mkdirs();
+            }
+            TextView project1 = (TextView) findViewById(R.id.project1);
+            project1.setText("-"+name_of_project);
+        }
     }
 }
