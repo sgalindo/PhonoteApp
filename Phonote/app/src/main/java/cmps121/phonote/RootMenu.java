5154dd140afda40d47829ecabbfc99796bfcfb2b
Salvador Galindo <sagalind@ucsc.edu>
2018-04-27 00:26:56 -0700

    sources can now be manually created. problem with rootmenu activity but should be fixed when I pull master.

--- Phonote/app/src/main/java/cmps121/phonote/RootMenu.java
+++ Phonote/app/src/main/java/cmps121/phonote/RootMenu.java
@@ -1,13 +1,13 @@
 package cmps121.phonote;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class RootMenu extends AppCompatActivity {
 
@@ -18,14 +18,16 @@ public class RootMenu extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        Button createSourceBtn = findViewById(R.id.btn_createSource);
+        createSourceBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                Intent createSourceIntent = new Intent(RootMenu.this,
+                        CreateSourceActivity.class);
+                startActivity(createSourceIntent);
             }
         });
+
     }
 
     @Override
