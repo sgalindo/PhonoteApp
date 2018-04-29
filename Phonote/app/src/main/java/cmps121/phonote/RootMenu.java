9e205ea7b707e5b7fe42d4691f55b55947554451
Buzz Tilford <tilford20@gmail.com>
2018-04-29 12:16:40 -0700

    Revert "Merge branch 'connectToGoogle' into directory_system"
    This reverts commit dda241b30f1a5d1180b8df35332f67a651ce8f62, reversing
    changes made to dda7686af18b1a73181bc656deb48ea89376d12e.

--- Phonote/app/src/main/java/cmps121/phonote/RootMenu.java
+++ Phonote/app/src/main/java/cmps121/phonote/RootMenu.java
@@ -16,15 +16,6 @@ import android.widget.EditText;
 import android.content.DialogInterface;
 import android.text.InputType;
 
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.SignInButton;
-import com.google.android.gms.drive.Drive;
-import com.google.android.gms.auth.api.Auth;
-import com.google.android.gms.tasks.Task;
-
 public class RootMenu extends AppCompatActivity {
 
     @Override
@@ -33,7 +24,6 @@ public class RootMenu extends AppCompatActivity {
         setContentView(R.layout.activity_root_menu);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        final GoogleSignInClient mGoogleSignInClient = buildGoogleSignInClient();
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -82,44 +72,6 @@ public class RootMenu extends AppCompatActivity {
 
             }
         });
-
-
-
-        SignInButton signIn = findViewById(R.id.sign_in_button);
-        signIn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                int RC_SIGN_IN = 100;
-                Intent signInIntent = mGoogleSignInClient.getSignInIntent();
-                startActivityForResult(signInIntent, RC_SIGN_IN);
-            }
-        });
-
-    }
-
-
-
-
-    @Override
-    protected void onStart() {
-        // Check for existing Google Sign In account, if the user is already signed in
-        // the GoogleSignInAccount will be non-null.
-        super.onStart();
-
-        Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-        if (task){
-            SignInButton signIn = findViewById(R.id.sign_in_button);
-            signIn.setVisibility(0);
-        }
-        else {
-            SignInButton signIn = findViewById(R.id.sign_in_button);
-            signIn.setVisibility(1);
-
-
-        }
-
-        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
-        //updateUI(account);
     }
 
     @Override
@@ -142,13 +94,5 @@ public class RootMenu extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    private GoogleSignInClient buildGoogleSignInClient() {
-        GoogleSignInOptions signInOptions =
-                new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                        .requestScopes(Drive.SCOPE_FILE)
-                        .build();
-        return GoogleSignIn.getClient(this, signInOptions);
     }
 }
