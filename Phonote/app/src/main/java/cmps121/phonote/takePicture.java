56f0080a9f55b0fb47201e95eb91231041f571ed
Dustin Halsey <dhalsey@ucsc.edu>
2018-05-01 20:17:06 -0700

    Sorta working? need to look into it more. camera is being save super low rez/ crashes

--- Phonote/app/src/main/java/cmps121/phonote/takePicture.java
+++ Phonote/app/src/main/java/cmps121/phonote/takePicture.java
@@ -1,66 +1,91 @@
 package cmps121.phonote;
 
-import android.graphics.PixelFormat;
 import android.hardware.Camera;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
-import android.hardware.Camera;
+import android.view.View;
+import android.widget.FrameLayout;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
-public class takePicture extends AppCompatActivity implements SurfaceHolder.Callback{
-    Camera camera;
-    SurfaceView surfaceCamera; //where the camera will be displayed
-    SurfaceHolder holderCamera; //gets the frame of the surfaceView
-    boolean isPreviewing = false; //keeps track if the camera is already opened
+public class takePicture extends AppCompatActivity{
+    private Camera mCamera = null;
+    private FrameLayout mFrame = null;
+    private SurfaceView mSurface = null;
+    private SurfaceHolder mHolder = null;
+    CameraFunction showCamera;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_take_picture);
+        mFrame = findViewById(R.id.frameCamera);
 
-        getWindow().setFormat(PixelFormat.UNKNOWN);//?
-        surfaceCamera = (SurfaceView) findViewById(R.id.surfaceCamera);
-        holderCamera = surfaceCamera.getHolder();
-        holderCamera.addCallback(this);
-        holderCamera.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS); //?
+        try {
+            mCamera = Camera.open();
+        } catch (Exception e){
+            //Couldn't open camera
+        }
 
-
-
-
+        showCamera = new CameraFunction(this, mCamera); //holds the display of the camera
+        mFrame.addView(showCamera);
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        if (isPreviewing == false) {
-            camera = Camera.open(); //Why does this error???
-            try{
-                if (camera != null){
-                    camera.setPreviewDisplay(surfaceCamera.getHolder());
-                    camera.startPreview();
-                    isPreviewing = true;
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+
+
+    //Handles taking a picture. called when the take picture button is pressed
+    public void captureImage(View v){
+        if(mCamera!=null){
+            mCamera.takePicture(null,null,mPictureCallback);//takes the picture and calls onPictureTaken
+            System.out.println("Success");
+            //mCamera.startPreview();
         }
     }
 
-    @Override
-    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+    //Handles saving a picture after taking it
+    Camera.PictureCallback mPictureCallback = new Camera.PictureCallback(){
+        @Override
+        public void onPictureTaken(byte[] data, Camera camera){
+            File pictureFile = getOutputMediaFile();
+            if (pictureFile == null) {
+                return;
+            }
+            try {
+                FileOutputStream fos = new FileOutputStream(pictureFile);
+                fos.write(data);
+                fos.close();
+                //mCamera.release();
+            }  catch (IOException e) {
+            }
+        }
+    };
 
+    private static File getOutputMediaFile() {
+
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "Phonote");
+        if (!mediaStorageDir.exists()) {
+            if (!mediaStorageDir.mkdirs()) {
+                Log.d("MyCameraApp", "failed to create directory");
+                return null;
+            }
+        }
+
+
+        // Create a media file name
+        String timeStamp = Long.toString (System.currentTimeMillis()/1000);
+
+        File mediaFile;
+        mediaFile = new File(mediaStorageDir.getPath() + File.separator
+                + "IMG_" + timeStamp + ".png");
+
+        return mediaFile;
     }
 
-    @Override
-    public void surfaceDestroyed(SurfaceHolder holder) {
-
-    }
-
-    @Override
-    public void surfaceCreated(SurfaceHolder holder) {
-
-    }
 }
 
