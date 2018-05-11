e440a1239fbff491fb10a8a6920085740e6521be
Salvador Galindo <sagalind@ucsc.edu>
2018-05-11 00:46:56 -0700

    sources now save in a file called sources.ser and are serialized/deserialized exactly the same way as in HW2. They can be manually created, viewed or deleted.

--- Phonote/app/src/main/java/cmps121/phonote/SourceData.java
+++ Phonote/app/src/main/java/cmps121/phonote/SourceData.java
@@ -3,38 +3,9 @@ package cmps121.phonote;
 import java.io.Serializable;
 
 public class SourceData implements Serializable {
-    private String title;
-    private String author;
-    private String publisher;
-    private String city;
-    private String year;
-
-    public SourceData(String title, String author, String publisher,
-                      String city, String year) {
-        this.title = title;
-        this.author = author;
-        this.publisher = publisher;
-        this.city = city;
-        this.year = year;
-    }
-
-    public String getTitle() {
-        return this.title;
-    }
-
-    public String getAuthor() {
-        return this.author;
-    }
-
-    public String getPublisher() {
-        return this.publisher;
-    }
-
-    public String getCity() {
-        return this.city;
-    }
-
-    public String getYear() {
-        return this.year;
-    }
+    public String title;
+    public String author;
+    public String publisher;
+    public String city;
+    public String year;
 }
