568164024804f4bb5c8e81a549bd35685bb71001
Dustin Halsey <dhalsey@ucsc.edu>
2018-05-15 23:51:47 -0700

    HQ images now saved.  Rotate image added. crop WIP

--- Phonote/app/src/main/java/cmps121/phonote/RootMenu.java
+++ Phonote/app/src/main/java/cmps121/phonote/RootMenu.java
@@ -3,10 +3,15 @@ package cmps121.phonote;
 import android.app.Activity;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.content.pm.ResolveInfo;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.StrictMode;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -16,6 +21,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.Toast;
+import android.graphics.Matrix;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
@@ -28,6 +34,8 @@ import com.google.android.gms.drive.Drive;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.lang.reflect.Method;
+
 
 public class RootMenu extends AppCompatActivity {
 
@@ -35,12 +43,25 @@ public class RootMenu extends AppCompatActivity {
 
     private int REQ_CODE_CAMERA = 1;
     private int REQ_CODE_CROP = 2;
-    private Uri HQimageUri = null; //needed to get high quality image instead of thumbnail
+    private Uri HQimageUri; //needed to get high quality image instead of thumbnail
+    private File imageFile; //hol
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_root_menu);
+
+        //Cheeky fix needed to avoid Uri crashes.
+        //Disables the check for security risk if other apps can access the Uri. we don't care if this happens
+        if(Build.VERSION.SDK_INT>=24){
+            try{
+                Method m = StrictMode.class.getMethod("disableDeathOnFileUriExposure");
+                m.invoke(null);
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+        }
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         final GoogleSignInClient mGoogleSignInClient = buildGoogleSignInClient();
@@ -57,15 +78,15 @@ public class RootMenu extends AppCompatActivity {
         });
 
         //Click activity for camera button
-        ImageButton btnCamera =  findViewById(R.id.btnCamera);
+        ImageButton btnCamera = findViewById(R.id.btnCamera);
         btnCamera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                HQimageUri = Uri.fromFile(getOutputMediaFile());
+                imageFile = getOutputMediaFile();
+                HQimageUri = Uri.fromFile(imageFile);
                 Intent goToCamera = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE); //
-                //goToCamera.putExtra(MediaStore.EXTRA_OUTPUT, HQimageUri);
-                startActivityForResult(goToCamera,REQ_CODE_CAMERA);
+                goToCamera.putExtra(MediaStore.EXTRA_OUTPUT, HQimageUri);
+                startActivityForResult(goToCamera, REQ_CODE_CAMERA);
             }
         });
 
@@ -85,60 +106,50 @@ public class RootMenu extends AppCompatActivity {
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data){
-        super.onActivityResult(requestCode,resultCode,data);
-        if(resultCode == Activity.RESULT_OK){
-            if(requestCode == REQ_CODE_CAMERA){ //If returning from camera
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == REQ_CODE_CAMERA) { //If returning from camera
                 //Uri pic = data.getData(); //gets the picture that was returned to pass to the cropper
-                //cropImage(pic);  BROKEN FOR NOW. FIX THIS TO ALLOW CROPPING
 
-                File pictureFile = getOutputMediaFile(); //gets file location and name that the photo will be saved as
-                if (pictureFile == null) return;
-                Bitmap bitmap = (Bitmap)data.getExtras().get("data");
+
+                if (imageFile == null) {
+                    Toast.makeText(this, "No Valid file path. Picture not saved", Toast.LENGTH_LONG).show();
+                    return; //No picture could be saved
+                }
+
+                BitmapFactory.Options options = new BitmapFactory.Options(); //used to set bitmap options (specifically to lower image file size)
+                options.inSampleSize = 2; //reduces file size to avoid out of memory issues (power of 2. 2 = 1/2 image size, 4 = 1/4 image size
+
+                Bitmap bitmap = BitmapFactory.decodeFile(HQimageUri.getPath(),options);
+                bitmap = editImage.rotateBitmap(bitmap,90); //automatically rotates the image 90degrees as I found all images started sideways
 
                 try {
-                    FileOutputStream fos = new FileOutputStream(pictureFile);
+                    FileOutputStream fos = new FileOutputStream(imageFile);
                     bitmap.compress(Bitmap.CompressFormat.PNG, 100, fos);
                     fos.flush();
                     fos.close();
-                }  catch (IOException e) {
+                    Intent cropIntent = new Intent(RootMenu.this, editImage.class); //intent to move to the crop activity
+                    cropIntent.putExtra("image",imageFile.getAbsolutePath()); //adds the image location to be passed to the crop
+                    startActivityForResult(cropIntent,REQ_CODE_CROP);
+                } catch (IOException e) {
                     e.printStackTrace();
                 }
 
             }
 
-            if(requestCode == REQ_CODE_CROP) {// If returning from crop
+            if (requestCode == REQ_CODE_CROP) {// If returning from crop
 
             }
         }
     }
 
-    public void cropImage(Uri pic){
-        try {
-            //call the standard crop action intent (the user device may not support it)
-            Intent cropIntent = new Intent("com.android.camera.action.CROP");
-            //indicate image type and Uri
-            cropIntent.setDataAndType(pic, "image/*");
-            //set crop properties
-            cropIntent.putExtra("crop", "true");
-            //retrieve data on return
-            cropIntent.putExtra("return-data", true);
-            //start the activity - we handle returning in onActivityResult
-            startActivityForResult(cropIntent, REQ_CODE_CROP);
-        }
-        catch(ActivityNotFoundException anfe){
-            //display an error message
-            String errorMessage = "Whoops - your device doesn't support the crop action!";
-            Toast toast = Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT);
-            toast.show();
-        }
-
     protected void onResume() {
         super.onResume();
-            SignInButton signIn = findViewById(R.id.sign_in_button);
-            signIn.setVisibility(View.INVISIBLE);
-            Button signOut = findViewById(R.id.sign_out_button);
-            signOut.setVisibility(View.VISIBLE);
+        SignInButton signIn = findViewById(R.id.sign_in_button);
+        signIn.setVisibility(View.INVISIBLE);
+        Button signOut = findViewById(R.id.sign_out_button);
+        signOut.setVisibility(View.VISIBLE);
     }
 
     @Override
@@ -150,13 +161,12 @@ public class RootMenu extends AppCompatActivity {
 
         GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
 
-        if (account != null){
+        if (account != null) {
             SignInButton signIn = findViewById(R.id.sign_in_button);
             signIn.setVisibility(View.INVISIBLE);
             Button signOut = findViewById(R.id.sign_out_button);
             signOut.setVisibility(View.VISIBLE);
-        }
-        else {
+        } else {
             SignInButton signIn = findViewById(R.id.sign_in_button);
             signIn.setVisibility(View.VISIBLE);
             Button signOut = findViewById(R.id.sign_out_button);
@@ -178,8 +188,8 @@ public class RootMenu extends AppCompatActivity {
 
         imgToTxt = (Button) findViewById(R.id.button_image_to_text);
         imgToTxt.setOnClickListener(
-                new Button.OnClickListener(){
-                    public void onClick(View v){
+                new Button.OnClickListener() {
+                    public void onClick(View v) {
                         Intent intent = new Intent(getApplicationContext(), ImageToText.class);
                         startActivity(intent);
                     }
@@ -206,7 +216,6 @@ public class RootMenu extends AppCompatActivity {
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 
@@ -220,7 +229,6 @@ public class RootMenu extends AppCompatActivity {
 
     //gets the media location to save photos and returns the media file that the photo will be saved ass
     private static File getOutputMediaFile() {
-
         File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "Phonote");
         if (!mediaStorageDir.exists()) {
             if (!mediaStorageDir.mkdirs()) {
@@ -228,15 +236,12 @@ public class RootMenu extends AppCompatActivity {
                 return null;
             }
         }
-        // Create a media file name
-        String timeStamp = Long.toString (System.currentTimeMillis()/1000);
+        String timeStamp = Long.toString(System.currentTimeMillis() / 1000); //gets a timestamp for a unique photo name
 
-        File mediaFile;
-        mediaFile = new File(mediaStorageDir.getPath() + File.separator
-                + "IMG_" + timeStamp + ".png");
-
+        File mediaFile = new File(mediaStorageDir.getPath() + File.separator + "IMG_" + timeStamp + ".png"); // Create a media file name
         return mediaFile;
     }
+
 
 }
 
---
+++ Phonote/app/src/main/java/cmps121/phonote/editImage.java
@@ -0,0 +1,70 @@
+package cmps121.phonote;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class editImage extends AppCompatActivity {
+    private Bitmap bmp = null;
+    private String picLoc;
+    private ImageView imageView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_image);
+        imageView = findViewById(R.id.ivPic);
+
+        picLoc = getIntent().getStringExtra("image");
+        bmp = BitmapFactory.decodeFile(picLoc);
+
+        imageView.setImageBitmap(bmp);
+
+
+    }
+
+    public void rotateRight(View view){
+        bmp = rotateBitmap(bmp, 90);
+        imageView.setImageBitmap(bmp);
+    }
+
+    public void rotateLeft(View view){
+        bmp = rotateBitmap(bmp, -90);
+        imageView.setImageBitmap(bmp);
+    }
+
+    public void confirmChanges(View view){
+        try {
+            File fileOut = new File(picLoc);
+            FileOutputStream fos = new FileOutputStream(fileOut);
+            bmp.compress(Bitmap.CompressFormat.PNG, 100, fos);
+            fos.flush();
+            fos.close();
+        }catch (FileNotFoundException e){
+            Toast.makeText(this, "No Valid file path. Picture not saved", Toast.LENGTH_LONG).show();
+        } catch (IOException e) {
+            Toast.makeText(this, "IOException", Toast.LENGTH_LONG).show();
+        }
+
+        Intent intentRoot = new Intent(this,RootMenu.class);
+        Toast.makeText(this, "Image Saved =)", Toast.LENGTH_LONG).show();
+        startActivity(intentRoot);
+    }
+
+    public static Bitmap rotateBitmap(Bitmap source, float angle)
+    {
+        Matrix matrix = new Matrix();
+        matrix.postRotate(angle);
+        return Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(), matrix, true);
+    }
+}
