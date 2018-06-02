c08e200ffed0b31f1a83ba9ffd20cb6e77ce3929
AlfredoCuevas <cuevasalfredo0@gmail.com>
2018-06-02 13:33:04 -0700

    finished creating the list view for notes. Added an on click listener to the elements that opens up an activity. This activity needs to be programmed to show editText boxes with the note information

---
+++ Phonote/app/src/main/java/cmps121/phonote/ExamineNoteActivity.java
@@ -0,0 +1,13 @@
+package cmps121.phonote;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ExamineNoteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_examine_note);
+    }
+}
